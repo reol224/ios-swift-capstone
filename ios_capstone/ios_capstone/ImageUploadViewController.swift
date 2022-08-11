4e9161fce3d176317b5902c2c53f84e010e254ab
zhoufengfun <53028077+zhoufengfun@users.noreply.github.com>
2022-08-12 04:07:52 +0800

    position info from selected picture
    auto get position info from selected picture
    info.plist set photo Library access

--- ios_capstone/ios_capstone/ImageUploadViewController.swift
+++ ios_capstone/ios_capstone/ImageUploadViewController.swift
@@ -6,17 +6,21 @@
 //
 
 import UIKit
+import PhotosUI
 
 class ImageUploadViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var position: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func tapImageView(_ sender: UITapGestureRecognizer) {
-        let picker = UIImagePickerController();
+        let photoLibrary = PHPhotoLibrary.shared()
+        let configuration = PHPickerConfiguration(photoLibrary: photoLibrary)
+        let picker = PHPickerViewController(configuration: configuration)
         picker.delegate = self
         present(picker, animated: true, completion: nil)
     }
@@ -24,14 +28,43 @@ class ImageUploadViewController: UIViewController {
 }
 
 // set delegate for picker
-extension ImageUploadViewController : UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        // dismiss photo library
-        picker.dismiss(animated: false, completion: nil)
-        // set imageView the selected image
-        imageView.image = info[.originalImage] as? UIImage
-        
-        // get selected image's metaData: Asset, creation Data, Location
-        
+extension ImageUploadViewController : UIImagePickerControllerDelegate, UINavigationControllerDelegate, PHPickerViewControllerDelegate {
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        picker.dismiss(animated: true)
+        guard !results.isEmpty else {
+            return
+          }
+//          let identifiers = results.compactMap(\.assetIdentifier)
+//          let fetchResult = PHAsset.fetchAssets(withLocalIdentifiers: identifiers, options: nil)
+//          print(fetchResult)
+          let imageResult = results[0]
+            // change imageVIew
+            imageResult.itemProvider.loadObject(ofClass: UIImage.self, completionHandler: { (object, error) in
+               if let image = object as? UIImage {
+                  DispatchQueue.main.async {
+                     // Use UIImage
+                    self.imageView.image = image
+                  }
+               }
+            })
+          // get image Assets info
+          if let assetId = imageResult.assetIdentifier {
+            let assetResults = PHAsset.fetchAssets(withLocalIdentifiers: [assetId], options: nil)
+            position.text = "\(String(describing: assetResults.firstObject?.location?.coordinate.latitude ?? 0.0000))   \(String(describing: assetResults.firstObject?.location?.coordinate.longitude ?? 0.0000))"
+          
+//            print(assetResults.firstObject?.creationDate ?? "No date")
+//            print(assetResults.firstObject?.location?.coordinate.latitude ?? "No location")
+          }
+                    
     }
+    
+//    public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+//        // dismiss photo library
+//        picker.dismiss(animated: false, completion: nil)
+//        // set imageView the selected image
+//        imageView.image = info[.originalImage] as? UIImage
+//
+//        // get selected image's metaData: Asset, creation Data, Location
+//
+//    }
 }
