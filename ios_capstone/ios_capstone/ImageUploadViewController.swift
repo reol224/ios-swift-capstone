b01e833925887f10fe67538500db3a9942564988
zhoufengfun <53028077+zhoufengfun@users.noreply.github.com>
2022-08-12 19:06:35 +0800

    upload image
    upload image api is ok
    but image is big ,and network is slow

--- ios_capstone/ios_capstone/ImageUploadViewController.swift
+++ ios_capstone/ios_capstone/ImageUploadViewController.swift
@@ -14,10 +14,13 @@ class ImageUploadViewController: UIViewController {
     @IBOutlet weak var position: UITextField!
     @IBOutlet weak var infoShow: UILabel!
     @IBOutlet weak var itemTitle: UITextField!
-    @IBOutlet weak var keywords: UITextField!
     @IBOutlet weak var itemDescribe: UITextField!
     var latitude = "0.0000"
     var longitude = "0.0000"
+    var imageSelected = UIImage()
+    var imageSelectedName = ""
+    var uploadImgUrl = ""
+    var uploadImgTbUrl = ""
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -39,58 +42,62 @@ class ImageUploadViewController: UIViewController {
         return jsonStr! as String
     }
     
-//    func getUrlFromS3() {
-//        let session = URLSession(configuration: .default)
-//        let url = "https://capstone.freeyeti.net/api/photos/upload"
-//        var request = URLRequest(url: URL(string: url)!)
-//        request.setValue("multipart/form-data", forHTTPHeaderField: "Content-Type")
-//        request.httpMethod = "POST"
-//        // post dict
-//        let postData = ["photo": imageView] as [String : Any]
-//
-//        let postString = convertDictionaryToJSONString(dict: postData as NSDictionary)
-//        request.httpBody = postString.data(using: .utf8)
-//        // task for post
-//        let task = session.dataTask(with: request) {(data, response, error) in
-//            do {
-//                let r = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as! NSDictionary
-//                if r.allKeys.count > 2 {
-//                    self.infoShow.text = "Successfully!"
-//                }
-//            } catch {
-//                print("network error")
-//                return
-//            }
-//        }
-//        task.resume()
-//    }
+    func uploadImgForUrl(paramName: String, fileName: String, image: UIImage) {
+        let url = URL(string: "https://capstone.freeyeti.net/api/photos/upload")
+
+        // generate boundary string using a unique per-app string
+        let boundary = UUID().uuidString
+
+        let session = URLSession.shared
+
+        // Set the URLRequest to POST and to the specified URL
+        var urlRequest = URLRequest(url: url!)
+        urlRequest.httpMethod = "POST"
+
+        // Set Content-Type Header to multipart/form-data, this is equivalent to submitting form data with file upload in a web browser
+        // And the boundary is also set here
+        urlRequest.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+
+        var data = Data()
+
+        // Add the image data to the raw http request data
+        data.append("\r\n--\(boundary)\r\n".data(using: .utf8)!)
+        data.append("Content-Disposition: form-data; name=\"\(paramName)\"; filename=\"\(fileName)\"\r\n".data(using: .utf8)!)
+        data.append("Content-Type: image/png\r\n\r\n".data(using: .utf8)!)
+        data.append(image.pngData()!)
+
+        data.append("\r\n--\(boundary)--\r\n".data(using: .utf8)!)
+        //print(data)
+        // Send a POST request to the URL, with the data we created earlier
+        session.uploadTask(with: urlRequest, from: data, completionHandler: { responseData, response, error in
+            if error == nil {
+                let jsonData = try? JSONSerialization.jsonObject(with: responseData!, options: .allowFragments)
+                if let json = jsonData as? [String: Any] {
+                    self.uploadImgUrl = json["image_url"]! as! String
+                    self.uploadImgTbUrl = json["thumb_url"]! as! String
+                    print(json)
+                }
+            }
+        }).resume()
+    }
     
-    // upload file start
-    @IBAction func uploadImage(_ sender: Any) {
-        
-        if itemTitle.text == "" || keywords.text == "" || itemDescribe.text == "" {
-            infoShow.text = "please fill in all field"
-            return
-        }
-        // create a URLSession
-        let session = URLSession(configuration: .default)
-       
-        
+    func uploadImgToDatabase(url: String, thumb_url: String){
         // step2 upload info to database
-        
         // set url
+        let session = URLSession(configuration: .default)
         let url = "https://capstone.freeyeti.net/api/photos/"
         var request = URLRequest(url: URL(string: url)!)
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         request.httpMethod = "POST"
         // post dict
-        let postData = ["title":itemTitle.text!,"description":itemDescribe.text!,"keywords": keywords.text!,"url": "https://capstone.freeyeti.net/media/9c412b24-fc91-44e1-b87a-fdb439994b2b.jpg",
-                        "thumb_url": "https://capstone.freeyeti.net/media/9c412b24-fc91-44e1-b87a-fdb439994b2b_thumb.jpg","position": [
+        let postData = ["title":itemTitle.text!,"description":itemDescribe.text!,"keywords": itemTitle.text!,"url": "https://capstone.freeyeti.net\(url)",
+                        "thumb_url": "https://capstone.freeyeti.net\(thumb_url)","position": [
                             "latitude": latitude,
                             "longitude": longitude
                         ]] as [String : Any]
 
         let postString = convertDictionaryToJSONString(dict: postData as NSDictionary)
+        //print(postString)
         request.httpBody = postString.data(using: .utf8)
         // task for post
         let task = session.dataTask(with: request) { (data, response, error) in
@@ -103,6 +110,29 @@ class ImageUploadViewController: UIViewController {
             }
         }
         task.resume()
+    }
+    
+    // upload file start
+    @IBAction func uploadImage(_ sender: Any) {
+        
+        if itemTitle.text == "" || itemDescribe.text == "" {
+            infoShow.text = "please fill in all field"
+            return
+        }
+        infoShow.text = "uploading..."
+        
+        if uploadImgUrl != "" && uploadImgTbUrl != "" {
+            //step2 upload database
+            uploadImgToDatabase(url: uploadImgUrl, thumb_url: uploadImgTbUrl)
+            
+            infoShow.text = "Upload Successfully"
+            uploadImgUrl = ""
+            uploadImgTbUrl = ""
+            itemTitle.text = ""
+            itemDescribe.text = ""
+            position.text = ""
+        }
+        
     }
     
 }
@@ -119,12 +149,28 @@ extension ImageUploadViewController : PHPickerViewControllerDelegate {
             // change imageVIew
             imageResult.itemProvider.loadObject(ofClass: UIImage.self, completionHandler: { (object, error) in
                if let image = object as? UIImage {
+                self.imageSelected = image
+                imageResult.itemProvider.loadFileRepresentation(forTypeIdentifier: UTType.image.identifier) { (url, error) in
+                        guard let url = url else {
+                            return
+                        }
+                    //print( url.absoluteURL)
+                    // set selected image url and url name
+                    self.imageSelectedName = url.lastPathComponent
+                    self.uploadImgForUrl(paramName: "photo", fileName: url.lastPathComponent, image: image)
+                    }
                   DispatchQueue.main.async {
                      // Use UIImage
                     self.imageView.image = image
+                    //step1 create a URLSession
+                    
                   }
                }
             })
+        
+        
+        
+        
           // get image Assets info
           if let assetId = imageResult.assetIdentifier {
             let assetResults = PHAsset.fetchAssets(withLocalIdentifiers: [assetId], options: nil)
@@ -132,7 +178,7 @@ extension ImageUploadViewController : PHPickerViewControllerDelegate {
             latitude = "\(String(describing: assetResults.firstObject?.location?.coordinate.latitude ?? 0.0000))"
             longitude = "\(String(describing: assetResults.firstObject?.location?.coordinate.longitude ?? 0.0000))"
             
-            print(assetResults.firstObject?.description ?? "No date")
+            //print(assetResults.firstObject?.description ?? "No date")
 //            print(assetResults.firstObject?.creationDate ?? "No date")
 //            print(assetResults.firstObject?.location?.coordinate.latitude ?? "No location")
           }
